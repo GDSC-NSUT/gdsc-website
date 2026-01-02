5589988e3125e9baec145f733c83fc69de1462c5
zeditme <eeyeriamd@gmail.com>
2026-01-02 14:53:43 +0530

    play icon fix

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function HomePage() {
               >
                 <svg
                   viewBox="0 0 24 24"
-                  className="w-6 h-6 fill-black translate-x-[1px]"
+                  className="w-10 h-10 fill-black translate-x-[-1px]"
                 >
                   <path d="M8 5.14v13.72c0 .97 1.05 1.57 1.9 1.06l10.3-6.86c.8-.53.8-1.6 0-2.12L9.9 4.08c-.85-.51-1.9.09-1.9 1.06z" />
                 </svg>
